e1908732dc963f7917a562c01e73a8c8cc166b16
Anna <44573387+annfike@users.noreply.github.com>
2021-12-08 10:38:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 from dotenv import load_dotenv
 
 
-def raise_exception_vk(response):
+def raise_vk_exception(response):
     if 'error' in response:
         error_code = response['error']['error_code']
         error_message = response['error']['error_msg']
@@ -31,7 +31,7 @@ def fetch_comics(path, comics):
     return response['alt']
 
 
-def fetch_last_comic():
+def fetch_last_comic_number():
     url = 'https://xkcd.com/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
@@ -49,12 +49,12 @@ def get_wall_upload_server(vk_token, group_id):
     response = requests.post(url, params=payload)
     response.raise_for_status()
     response = response.json()
-    raise_exception_vk(response)
+    raise_vk_exception(response)
     url = response['response']['upload_url']
     return url
 
 
-def upload_pic_server(path, comics, url):
+def upload_pic_to_server(path, comics, url):
     with open(f'{path}/{comics}.png', 'rb') as file:
         files = {
             'photo': file,  
@@ -62,33 +62,33 @@ def upload_pic_server(path, comics, url):
         response = requests.post(url, files=files)
     response.raise_for_status()
     response = response.json()
-    raise_exception_vk(response)
+    raise_vk_exception(response)
     server = response['server']
     photo = response['photo']
-    vk_hash = response['hash']
-    return server, photo, vk_hash
+    pic_hash = response['hash']
+    return server, photo, pic_hash
 
 
-def save_pic_wall(server, photo, vk_hash, vk_token, group_id):
+def save_pic_to_group_albom(server, photo, pic_hash, vk_token, group_id):
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     payload = {
         'group_id': group_id,
         'server': server,
         'photo': photo,
-        'hash': vk_hash,
+        'hash': pic_hash,
         'access_token': vk_token,
         'v': '5.131',
     }
     response = requests.post(url, params=payload)
     response.raise_for_status()
     response = response.json()
-    raise_exception_vk(response)
+    raise_vk_exception(response)
     owner_pic_id = response['response'][0]['owner_id']
     media_id = response['response'][0]['id']
     return owner_pic_id, media_id
 
 
-def post_pic_wall(pic_message, owner_pic_id, media_id, vk_token, group_id):    
+def post_pic_on_wall(pic_message, owner_pic_id, media_id, vk_token, group_id):    
     group_id = int(group_id)
     url = 'https://api.vk.com/method/wall.post'
     payload = {
@@ -102,26 +102,26 @@ def post_pic_wall(pic_message, owner_pic_id, media_id, vk_token, group_id):
     response = requests.post(url, params=payload)
     response.raise_for_status()
     response = response.json()
-    raise_exception_vk(response)
+    raise_vk_exception(response)
     
 
 def main():
     load_dotenv()
     vk_token = os.getenv('VK_TOKEN')
     group_id = os.getenv('GROUP_ID')
-    comics = random.randint(1, fetch_last_comic())
+    comics = random.randint(1, fetch_last_comic_number())
     path = 'files'
     Path(path).mkdir(exist_ok=True)
+    pic_message = fetch_comics(path, comics)
     try:
-        pic_message = fetch_comics(path, comics)
         url = get_wall_upload_server(vk_token, group_id)
-        server, photo, vk_hash = upload_pic_server(path, comics, url)
-        owner_pic_id, media_id = save_pic_wall(server, photo, vk_hash, vk_token, group_id)
-        post_pic_wall(pic_message, owner_pic_id, media_id, vk_token, group_id)
+        server, photo, pic_hash = upload_pic_to_server(path, comics, url)
+        owner_pic_id, media_id = save_pic_to_group_albom(server, photo, pic_hash, vk_token, group_id)
+        post_pic_on_wall(pic_message, owner_pic_id, media_id, vk_token, group_id)
     finally:
-        file_to_rem = Path(f'{path}/{comics}.png')
-        file_to_rem.unlink()
-        Path(path).rmdir() 
+        filepath_to_rem = Path(f'{path}/{comics}.png')
+        filepath_to_rem.unlink()
+
 
     
 if __name__ == '__main__':
